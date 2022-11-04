4dba7e3d16696304f2b234c0ea8ff57c1b47e7ea
Dejan Kutle <kronikpillow@gmail.com>
2022-11-04 09:55:10 +0100

    default DOOM Emacs config

---
+++ config.el
@@ -0,0 +1,11 @@
+(setq user-full-name "KronikPillow"
+      user-mail-address "kronikpillow@gmail.com")
+
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+
+(setq doom-theme 'doom-one)
+
+(setq display-line-numbers-type t)
+
+(setq org-directory "~/org/")
